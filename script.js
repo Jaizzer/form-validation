fe4324bac5393473a7b028fb37b620c1ed636274
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-17 06:25:17 +0800

    feat: Add validate function with initial feature that sends error message if input is empty

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function isEmpty(input) {
     return input.value.trim('') === '';
 }
 
+function validate(input) {
+    // Access input' error mesage section.
+    const errorMessage = input.nextElementSibling;
+
+    // Check if input is empty.
+    if (isEmpty(input)) {
+        errorMessage.textContent = 'Input is missing!';
+    } else {
+        errorMessage.textContent = '';
+    }
+}
+
 function addValidation(form) {
     // Get all referece to the input
     const inputs = document.querySelectorAll('input');
