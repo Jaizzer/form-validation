1e9783d320d64af0751224e45621bbb7179a76f7
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-17 06:21:37 +0800

    feat: Addfunction for checking if input is empty

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function isEmpty(input) {
+    return input.value.trim('') === '';
+}
+
 function addValidation(form) {
     // Get all referece to the input
     const inputs = document.querySelectorAll('input');
