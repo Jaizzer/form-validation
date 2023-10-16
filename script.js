da29dd21babc929ae540be3607994042a5f6e418
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-16 09:07:41 +0800

    feat: Get reference to all input elements

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-function addValidation(form) {}
+function addValidation(form) {
+    // Get all referece to the input
+    const inputs = document.querySelectorAll('input');
+}
 addValidation(document.querySelector('form'));
