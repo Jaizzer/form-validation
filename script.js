7036d4d51378afa1dc0295240bbef123ae398a23
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-10-17 07:45:47 +0800

    feat: Add feature for checking the email, country code and zip code pattern

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function isEmpty(input) {
     return input.value.trim('') === '';
 }
 
+function isValidEmail(input) {
+    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(input.value);
+}
+
+function isValidCountryCode(input) {
+    return /^[A-Z]{2}$/.test(input.value);
+}
+
+function isValidZipCode(input) {
+    return /^\d{5}(-\d{4})?$/.test(input.value);
+}
+
 function validate(input) {
     // Access input' error mesage section.
     const errorMessage = input.nextElementSibling;
@@ -11,6 +23,23 @@ function validate(input) {
         errorMessage.textContent = 'Input is missing!';
     } else {
         errorMessage.textContent = '';
+    }
+
+    switch (input.id) {
+        case 'email':
+            errorMessage.textContent = isValidEmail(input) ? '' : 'Input a valid email address!';
+            break;
+
+        case 'country-code':
+            errorMessage.textContent = isValidCountryCode(input) ? '' : 'Input a valid country code!';
+            break;
+
+        case 'zip-code':
+            errorMessage.textContent = isValidZipCode(input) ? '' : 'Input a valid zip code!';
+            break;
+
+        default:
+            break;
     }
 }
 
